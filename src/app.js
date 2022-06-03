8e90f8bb7afa3a40c4e251f89a8cbe568a1dd0a0
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-06-03 21:30:01 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,28 +2,28 @@ let shop = document.getElementById('shop');
 
 let shopItemsData = [{
     id:"abc",
-    name:"casual Shirt",
+    name:"T-Shirt",
     price: 45,
     desc: "Lorem ipsum dolor sit amet ...",
     img: "img/1.jpg"
 },
  {
     id:"def",
-    name:"Office Shirt",
+    name:"Casual Shirt",
     price: 100,
     desc: "Lorem ipsum dolor sit amet ...",
     img: "img/2.jpg"
  },
   {
     id:"ghi",
-    name:"T-Shirt",
+    name:"Baby Shirt",
     price: 25,
     desc: "Lorem ipsum dolor sit amet ...",
     img: "img/3.jpg"
   },
    {
     id:"jkl",
-    name:"Mens Suit",
+    name:"Baby Shirt",
     price: 300,
     desc: "Lorem ipsum dolor sit amet ...",
     img: "img/4.jpg"
