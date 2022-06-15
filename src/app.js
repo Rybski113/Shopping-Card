ac4a8215d46e495706093045c125a1a747b0a216
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-06-15 14:16:21 +0200

    Local Storage
    Local Storage

--- src/app.js
+++ src/app.js
@@ -29,12 +29,13 @@ let shopItemsData = [{
     img: "img/4.jpg"
    }];
 
-   let basket = [];
+   let basket = JSON.parse(localStorage.getItem("data")) || []
 
 let generateShop = ()=> {
     return (shop.innerHTML = shopItemsData.map((x)=>{
 
         let {id,name,price,desc,img } = x;
+        let search = basket.find((x) => x.id === id) || []
 
         return  `
         <div id=product-id-${id} class="item">
@@ -46,7 +47,8 @@ let generateShop = ()=> {
                         <h2>${price}</h2>
                         <div class="buttons">
                             <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                            <div id=${id} class="quantity">0</div>
+                            <div id=${id} class="quantity">
+                            ${search.item === undefined? 0: search.item}</div>
                             <i onclick="increment(${id})" class="bi bi-plus-lg"></i> 
                         </div>
                     </div>
@@ -71,6 +73,7 @@ let increment = (id)=>{
         search .item +=1;
     }
 
+    localStorage.setItem("data", JSON.stringify(basket));
     
     //console.log(basket)
     update(selectedItem.id);
@@ -84,6 +87,8 @@ let decrement = (id)=>{
         search.item -=1;
     }
 
+    localStorage.setItem("data", JSON.stringify(basket));
+
     
     //console.log(basket)
     update(selectedItem.id);
@@ -96,5 +101,9 @@ let update = (id) => {
 };
 
 let calculation = () =>{
-    console.log(basket.map((x) => x.item).reduce((x,y)=> x+y,0))
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y)=> x+y,0)
+    
 };
+
+calculation();
