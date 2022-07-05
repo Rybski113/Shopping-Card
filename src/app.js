776d7bb300b46a47831f430e5036d0a2be11f145
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-05 18:57:05 +0200

    Shop data in different file

--- src/app.js
+++ src/app.js
@@ -1,33 +1,6 @@
 let shop = document.getElementById('shop');
 
-let shopItemsData = [{
-    id:"abc",
-    name:"T-Shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet ...",
-    img: "img/1.jpg"
-},
- {
-    id:"def",
-    name:"Casual Shirt",
-    price: 100,
-    desc: "Lorem ipsum dolor sit amet ...",
-    img: "img/2.jpg"
- },
-  {
-    id:"ghi",
-    name:"Baby Shirt",
-    price: 25,
-    desc: "Lorem ipsum dolor sit amet ...",
-    img: "img/3.jpg"
-  },
-   {
-    id:"jkl",
-    name:"Baby Shirt",
-    price: 300,
-    desc: "Lorem ipsum dolor sit amet ...",
-    img: "img/4.jpg"
-   }];
+
 
    let basket = JSON.parse(localStorage.getItem("data")) || []
 
