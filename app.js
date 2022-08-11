0ead8ca1529db47605d8ac824480c4416017a0b4
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-08-11 17:23:21 +0200

    shop items seen on site using return method

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ let shopItemsData = [
          img: "img/img-1.jpg"
     }, 
     {
-         id: "aaa",
+         id: "bbb",
          name: "Oficce Shirt",
          price: 75,
          desc: "Lorem Ipsum dolor sit amet...",
          img: "img/img-2.jpg"
     }, 
     {
-        id: "aaa",
+        id: "ccc",
         name: "T-Shirt",
         price: 15,
         desc: "Lorem Ipsum dolor sit amet...",
         img: "img/img-3.jpg"
     }, 
     {
-         id: "aaa",
-         name: "Suit",
+         id: "ddd",
+         name: "Men's Suit",
          price: 145,
          desc: "Lorem Ipsum dolor sit amet...",
          img: "img/img-4.jpg"
@@ -33,23 +33,24 @@ let shopItemsData = [
 
 
 let generateShop = ()=> {
-    return shop.innerHTML = `
-    <div class="item">
-    <img width="220" src="img/img-1.jpg" alt="">
-    <div class="details">
-        <h3>Casual Shirt</h3>
-        <p>Lorem ipsum, sit amet ...</p>
-        <div class="price-quantity">
-            <h2>$ 45</h2>
-            <div class="buttons">
-                <i class="fa-solid fa-minus"></i>
-              <div class="quantity">0</div>
-                <i class="fa-solid fa-plus"></i>
+    return (shop.innerHTML = shopItemsData.map((x)=>{
+        return `
+        <div class="item">
+        <img width="220" src="img/img-1.jpg" alt="">
+        <div class="details">
+            <h3>Casual Shirt</h3>
+            <p>Lorem ipsum, sit amet ...</p>
+            <div class="price-quantity">
+                <h2>$ 45</h2>
+                <div class="buttons">
+                    <i class="fa-solid fa-minus"></i>
+                  <div class="quantity">0</div>
+                    <i class="fa-solid fa-plus"></i>
+                </div>
             </div>
         </div>
-    </div>
- </div>
-    `
-}
+     </div>
+        `
+    }).join(""))}
 
 generateShop()
