1011a741981764143fb688c7551a0bd14515696d
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-08-12 16:23:27 +0200

    cart label

--- src/cart.js
+++ src/cart.js
@@ -13,5 +13,17 @@ let calculation = ()=> {
 calculation()
 
 let generateCartItems = ()=>{
-    
+    if(basket.length !== 0){}
+    else{
+        shoppingCart.innerHTML = ``
+        label.innerHTML = `
+        <h2>Cart is Empty</h2>
+        <a href="index.html">
+          <button class+"HomeBtn>Back to home</button>
+        </a>
+
+        `
+    }
 }
+
+generateCartItems()
