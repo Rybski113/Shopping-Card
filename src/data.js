e951678068d0ee188a6ce9a7673161fc75017330
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-08-12 20:07:47 +0200

    More products added

--- src/data.js
+++ src/data.js
@@ -26,4 +26,61 @@ let shopItemsData = [
          price: 145,
          desc: "Lorem Ipsum dolor sit amet...",
          img: "img/img-4.jpg"
-    }] 
+    },
+    {
+     id: "eee",
+     name: "Mens Tie",
+     price: 25,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-5.png",
+   },
+   {
+     id: "fff",
+     name: "Casual shoes",
+     price: 200,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-6.png",
+   },
+   {
+     id: "ggg",
+     name: "black suit",
+     price: 450,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-7.png",
+   },
+   {
+     id: "hhh",
+     name: "polo shirt",
+     price: 45,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-8.png",
+   },
+   {
+     id: "iii",
+     name: "denim shirt",
+     price: 85,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-9.png",
+   },
+   {
+     id: "jjj",
+     name: "denim pants",
+     price: 120,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-10.png",
+   },
+   {
+     id: "kkk",
+     name: "basic cap",
+     price: 35,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-11.png",
+   },
+   {
+     id: "lll",
+     name: "leather boots",
+     price: 350,
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+     img: "img/img-12.png",
+   },
+] 
